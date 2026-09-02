9111a161a7589c17b4198b23f8342618a9f05d42
Matthew Planchant <matt@logiccache.co.uk>
2026-09-02 21:48:29 +0100

    Playing around.... ⚽

--- players/player.py
+++ players/player.py
@@ -1,4 +1,23 @@
+from dataclasses import dataclass
+from datetime import date
+
+from players.position import Position
+
+
+@dataclass(frozen=True)
 class Player:
     first_name: str
     last_name: str
-    jersey: int
+    shirt_number: int
+    position: Position
+    dob: date
+
+    def calculate_age(self):
+        today = date.today()
+        if self.dob > today:
+            raise ValueError('Invalid date of birth.')
+        return today.year - self.dob.year - (
+            (today.month, today.day) < (self.dob.month, self.dob.day))
+
+    def is_goalkeeper(self):
+        return self.position == Position.GOALKEEPER
---
+++ players/position.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Position(Enum):
+    GOALKEEPER = 0
+    DEFENCE = 1
+    MIDFIELD = 2
+    ATTACK = 3
